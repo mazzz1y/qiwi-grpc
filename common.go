d2ee5d7eaf7d1a075acbe3eb76317ee3790a20c6
Dmitry Rubtsov <drubtsov@ronasit.com>
2019-03-08 19:46:23 +0600

    update structure

--- common.go
+++ common.go
@@ -1,27 +1,29 @@
 package main
 
-import "log"
-
 const nedeedPercentForPayment = 30
+const maxAmountPerWallet = 15000
 
+
+//todo check max wallet balance
+//todo
 //todo check if wallet is not blocked for payment
 func GetPaymentLinks(amount int64, comment string) ([]string) {
 	accounts := Account{}.List()
 	var links []string
 	comment = "321" //todo
-	var total int64
 	for _, acc := range accounts {
-		sum := acc.MaxAllowableBalance - acc.Balance
-		log.Println(acc.MaxAllowableBalance)
-		log.Println(acc.Balance)
-		log.Println(sum)
-		if  amount > sum/100*nedeedPercentForPayment   {
-			if amount - (total + sum) <= 0 {
-				links = append(links, acc.GetPaymentLink(int(amount-total), ""))
-				log.Println(links)
-				break
-			}
-			links = append(links, acc.GetPaymentLink(int(amount), ""))
+		walAvalPay := acc.MaxAllowableBalance - acc.Balance
+		if amount == 0 {
+			break
+		}
+		if walAvalPay > amount {
+			links = append(links, acc.GetPaymentLink(int(amount), comment))
+			amount-=amount
+			continue
+		} else if (walAvalPay < amount) && (walAvalPay/100*nedeedPercentForPayment < amount) {
+			links = append(links, acc.GetPaymentLink(int(walAvalPay), comment))
+			amount-=walAvalPay
+			continue
 		}
 	}
 
